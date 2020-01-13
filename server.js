4fa093bc5c45ba95a039b89a0560be11258bec34
Julien <juliencaron@protonmail.com>
2020-01-13 19:40:56 +0100

    Add socket.io part: hello user !

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express')
+const app = require('express')()
+const http = require('http').createServer(app)
 const session = require('cookie-session')
 const bodyParser = require('body-parser')
+const io = require('socket.io')(http)
 
-const app = express()
+/**
+ * 1) Express.js app
+ * - Routes
+ */
+
+// For form
 const urlencodedParser = bodyParser.urlencoded({ extended: false })
 
+// Data "storage" in session
 app.use(session({ secret: 'todotopsecret' }))
 
 // create empty todolist if doesn't exists
@@ -37,4 +45,21 @@ app.use((req, res, next) => {
   res.redirect('/todo')
 })
 
-app.listen(8080)
+/**
+ * 1) Socket.js part.
+ * Real time app part.
+ */
+io.on('connection', (socket) => {
+  socket.on('new_user', (name) => {
+    console.log(`New user connected named: ${name}`)
+    socket.userName = name
+
+    socket.broadcast.emit('has_new_user', name)
+  })
+
+  socket.on('disconnect', () => {
+    console.log(`${socket.userName || 'user'} disconnected`)
+  })
+})
+
+http.listen(8080, () => console.log('listening on *:8080'))
